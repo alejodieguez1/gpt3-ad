a84cd7441e9a876ad052c5d670d1b78f72217ab1
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-02-03 20:37:14 -0300

    feat: navbar styling finished

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import "./header.css";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
