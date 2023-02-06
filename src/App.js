f63f679af69706231b4e8c1598ba24bc769ac428
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-02-05 22:42:25 -0300

    feat: added animations to the project

--- src/App.js
+++ src/App.js
@@ -11,22 +11,38 @@ import {
 import { CTA, Brand, Navbar } from "./components";
 
 import "./App.css";
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="gradient__bg">
-        <Navbar />
-        <Header />
+    <AnimationOnScroll animateIn="animate__fadeInDown" duration="0.2s">
+      <div className="App">
+        <AnimationOnScroll animateIn="animate__fadeIn" duration="0.2s">
+          <div className="gradient__bg">
+            <Navbar />
+            <Header />
+          </div>
+        </AnimationOnScroll>
+        <AnimationOnScroll animateIn="animate__fadeIn" duration="0.2s">
+          <Brand />
+          <WhatGPT3 />
+        </AnimationOnScroll>
+        <AnimationOnScroll animateIn="animate__fadeIn" duration="0.2s">
+          <Features />
+        </AnimationOnScroll>
+        <AnimationOnScroll animateIn="animate__fadeIn" duration="0.2s">
+          <Possibility />
+          <CTA />
+        </AnimationOnScroll>
+        <AnimationOnScroll animateIn="animate__fadeIn" duration="0.2s">
+          <Blog />
+        </AnimationOnScroll>
+        <AnimationOnScroll animateIn="animate__fadeIn" duration="0.2s">
+          <Footer />
+        </AnimationOnScroll>
       </div>
-      <Brand />
-      <WhatGPT3 />
-      <Features />
-      <Possibility />
-      <CTA />
-      <Blog />
-      <Footer />
-    </div>
+    </AnimationOnScroll>
   );
 };
 
